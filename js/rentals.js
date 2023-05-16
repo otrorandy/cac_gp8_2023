9ef9d96f911707cfeadca4a7576ee1ce69e42ff0
pablo <pablo6031@gmail.com>
2023-05-16 11:26:00 -0300

    nuevo menu

--- js/rentals.js
+++ js/rentals.js
@@ -1,5 +1,26 @@
 
 $(function() {
+  $('#todos').on('click', function() {
+      
+    $('.card-sillones').show();
+    $('.card-escritorios').show();
+    $('.card-mesas').show();
+    $('.card-tecnologia').show();
+
+
+    $('#escritorios').removeClass("active-tab");
+    $('#mesas').removeClass("active-tab");
+    $('#tecnologia').removeClass("active-tab");
+    $('#sillones').removeClass("active-tab");
+
+    $(this).toggleClass("active-tab").find(".menulink").toggleClass("active");
+
+  });
+});
+
+
+
+$(function() {
     $('#sillones').on('click', function() {
         
         $('.card-sillones').show();
@@ -8,7 +29,7 @@ $(function() {
       $('.card-mesas').hide();
       $('.card-tecnologia').hide();
 
-
+      $('#todos').removeClass("active-tab");
       $('#escritorios').removeClass("active-tab");
       $('#mesas').removeClass("active-tab");
       $('#tecnologia').removeClass("active-tab");
@@ -29,6 +50,7 @@ $(function() {
       $('.card-sillones').hide();
       $('.card-tecnologia').hide();
 
+      $('#todos').removeClass("active-tab");
       $('#escritorios').removeClass("active-tab");
       $('#sillones').removeClass("active-tab");
       $('#tecnologia').removeClass("active-tab");
@@ -49,6 +71,7 @@ $(function() {
       $('.card-mesas').hide();
       $('.card-tecnologia').hide();
 
+      $('#todos').removeClass("active-tab");
       $('#sillones').removeClass("active-tab");
       $('#mesas').removeClass("active-tab");
       $('#tecnologia').removeClass("active-tab");
@@ -67,6 +90,7 @@ $(function() {
       $('.card-mesas').hide();
       $('.card-escritorios').hide();
 
+      $('#todos').removeClass("active-tab");
       $('#sillones').removeClass("active-tab");
       $('#mesas').removeClass("active-tab");
       $('#escritorios').removeClass("active-tab");
@@ -76,6 +100,22 @@ $(function() {
 
     });
   });
+
+
+
+
+var iframe = document.getElementById("header");
+
+// Evento cuando el cursor se posiciona sobre el iframe
+iframe.addEventListener("mouseover", function() {
+  iframe.style.height = "250px"; // Cambiar el valor del height cuando el cursor está sobre el iframe
+  console.log("no funciona");
+});
+
+// Evento cuando el cursor se retira del iframe
+iframe.addEventListener("mouseout", function() {
+  iframe.style.height = "100px" // Restaurar el valor inicial del height cuando el cursor se retira del iframe
+});
 
 
   
