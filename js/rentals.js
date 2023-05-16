68daa48e22cccf9ff0da71b4ae25c6517f18f126
pablo <pablo6031@gmail.com>
2023-05-16 01:33:48 -0300

    responsivo

--- js/rentals.js
+++ js/rentals.js
@@ -1,71 +1,82 @@
+
 $(function() {
     $('#sillones').on('click', function() {
+        
         $('.card-sillones').show();
       // Ocultar todos los contenidos
       $('.card-escritorios').hide();
+      $('.card-mesas').hide();
+      $('.card-tecnologia').hide();
+
+
+      $('#escritorios').removeClass("active-tab");
+      $('#mesas').removeClass("active-tab");
+      $('#tecnologia').removeClass("active-tab");
 
       $(this).toggleClass("active-tab").find(".menulink").toggleClass("active");
-      $(this).toggleClass("texto").find(".menulink").toggleClass("active");
-      
-      // Resaltar la opción seleccionada
-      $('.catalogo-menu li').removeClass('active');
-      $(this).addClass('active');
-      
-      // Obtener el índice de la opción seleccionada
-      var index = $(this).index();
-      
-      // Mostrar el contenido correspondiente al índice
-      $('.content').eq(index).show();
+
     });
   });
+
+
+
+  $(function() {
+    $('#mesas').on('click', function() {
+        
+        $('.card-mesas').show();
+      // Ocultar todos los contenidos
+      $('.card-escritorios').hide();
+      $('.card-sillones').hide();
+      $('.card-tecnologia').hide();
+
+      $('#escritorios').removeClass("active-tab");
+      $('#sillones').removeClass("active-tab");
+      $('#tecnologia').removeClass("active-tab");
+
+      $(this).toggleClass("active-tab").find(".menulink").toggleClass("active");
+
+    });
+  });
+
+
+
 
   $(function() {
     $('#escritorios').on('click', function() {
         $('.card-escritorios').show();
       // Ocultar todos los contenidos
       $('.card-sillones').hide();
-      
-      // Resaltar la opción seleccionada
-      $('.catalogo-menu li').removeClass('active');
-      $(this).addClass('active');
-      
-      // Obtener el índice de la opción seleccionada
-      var index = $(this).index();
-      
-      // Mostrar el contenido correspondiente al índice
-      $('.content').eq(index).show();
+      $('.card-mesas').hide();
+      $('.card-tecnologia').hide();
+
+      $('#sillones').removeClass("active-tab");
+      $('#mesas').removeClass("active-tab");
+      $('#tecnologia').removeClass("active-tab");
+
+
+      $(this).toggleClass("active-tab").find(".menulink").toggleClass("active");
+
+    });
+  });
+
+  $(function() {
+    $('#tecnologia').on('click', function() {
+        $('.card-tecnologia').show();
+      // Ocultar todos los contenidos
+      $('.card-sillones').hide();
+      $('.card-mesas').hide();
+      $('.card-escritorios').hide();
+
+      $('#sillones').removeClass("active-tab");
+      $('#mesas').removeClass("active-tab");
+      $('#escritorios').removeClass("active-tab");
+
+
+      $(this).toggleClass("active-tab").find(".menulink").toggleClass("active");
+
     });
   });
 
 
-
-
-
-
-
-
-  ///dddddddd
-
-  $(function() {
-    function slideMenu() {
-      var activeState = $("#menu-container .menu-list").hasClass("active");
-      $("#menu-container .menu-list").animate({left: activeState ? "0%" : "-100%"}, 400);
-    }
-    $("#menu-wrapper").click(function(event) {
-      event.stopPropagation();
-      $("#hamburger-menu").toggleClass("open");
-      $("#menu-container .menu-list").toggleClass("active");
-      slideMenu();
   
-      $("body").toggleClass("overflow-hidden");
-    });
-  
-    $(".menu-list").find(".accordion-toggle").click(function() {
-      $(this).next().toggleClass("open").slideToggle("fast");
-      $(this).toggleClass("active-tab").find(".menu-link").toggleClass("active");
-  
-      $(".menu-list .accordion-content").not($(this).next()).slideUp("fast").removeClass("open");
-      $(".menu-list .accordion-toggle").not(jQuery(this)).removeClass("active-tab").find(".menu-link").removeClass("active");
-    });
-  }); // jQuery load
-  
+
